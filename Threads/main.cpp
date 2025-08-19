e2bdf41dc4f563d59f595a311f47c182418158bb
Super User <eduardo.sanchez@cetys.edu.mx>
2025-08-19 09:15:28 +0000

    Finished threads homework

--- Threads/main.cpp
+++ Threads/main.cpp
@@ -1,20 +1,74 @@
-#include <cstdlib>
-#include <ctime>
-
-srand(time(0));
+#include <random>
+#include <thread>
+#include <print>
 
 class Adder {
-public: 
-    int add(){
-        int a = 1 + (rand() % (1000 - 1 + 1));
-        int b = 1 + (rand() % (1000 - 1 + 1));
+private:
+    int results[10];
 
-        return a + b;
+public: 
+    Adder(){
+        for(int i=0; i<10; i++){
+            results[i] = 0;
+        }
     }
-}
+
+    void add(int threadId){        
+        std::mt19937 rng(std::random_device{}());
+        std::uniform_int_distribution<int> dist(1, 1000);
+        
+        int result = 0;
+
+        for(int i=0; i<100; i++){
+            result += dist(rng);
+        }
+
+        std::println("Sum of thread {} = {}\n", threadId, result);
+
+        this->results[threadId] = result;
+    }
+
+    void compareAdds(){
+        int max = 0;
+        int index = 0;
+
+        std::thread thread1([&](){ add(0); });
+        std::thread thread2([&](){ add(1); });
+        std::thread thread3([&](){ add(2); });
+        std::thread thread4([&](){ add(3); });
+        std::thread thread5([&](){ add(4); });
+        std::thread thread6([&](){ add(5); });
+        std::thread thread7([&](){ add(6); });
+        std::thread thread8([&](){ add(7); });
+        std::thread thread9([&](){ add(8); });
+        std::thread thread10([&](){ add(9); });
+
+        thread1.join();
+        thread2.join();
+        thread3.join();
+        thread4.join();
+        thread5.join();
+        thread6.join();
+        thread7.join();
+        thread8.join();
+        thread9.join();
+        thread10.join();
+
+        for(int i=0; i<10; i++){
+            if(results[i] > max){
+                max = results[i];
+                index = i;
+            }
+        }
+
+        std::println("Thread {} has the highest score with {}\n", index, max);
+    }
+};
 
 int main(){
-    podman run -d --replace --name CppDevContainer --network testnet -v /mnt/c/temporal/projects/:/root:z -p 20202:22 -p 8080:8080 cpp_dev_img:latest
+    Adder adder;
+
+    adder.compareAdds();
 
     return 0;
 }
